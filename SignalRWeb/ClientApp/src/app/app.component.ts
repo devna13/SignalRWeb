690821146dc0ef31a9cd12f6e8952c222166c9e9
VivekSingh <viveksi@vfsglobal.com>
2018-06-05 11:10:20 +0530

    Matched version of @aspnet/signalr and .NET core version to 1.0.0-preview1-final

--- SignalRWeb/ClientApp/src/app/app.component.ts
+++ SignalRWeb/ClientApp/src/app/app.component.ts
@@ -1,39 +1,26 @@
 import {Component} from '@angular/core';
 import {Message} from 'primeng/api';
-import {HubConnection, HubConnectionBuilder, IHubProtocol} from '@aspnet/signalr';
+import { HubConnection } from '@aspnet/signalr';
 
 @Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
-  public _hubConnection : HubConnection;
+  public _hubConnection : signalR.HubConnection;
 
   msgs : Message[] = [];
 
   constructor() {}
 
-  ngOnInit() : void {
-    let builder = new HubConnectionBuilder();
-    // this._hubConnection =
-    // builder.withUrl('https://localhost:44374/notification/notify').build();
-    this._hubConnection = builder
-      .withUrl('/notifyhub')      
-      .build();
-
-    this
-      ._hubConnection
+  ngOnInit() : void {    
+    this._hubConnection = new HubConnection('http://localhost:49446/notifyhub');
+    this._hubConnection
       .start()
       .then(() => console.log('Connection started!'))
-      .catch(err => console.log('Error :', err));;
-    // this._hubConnection   .start()   .then(() => console.log('Connection
-    // started!'))   .catch(err => console.log('Error while establishing connection
-    // :('));
-
+      .catch(err => console.log(err));
+  
     this
       ._hubConnection
       .on('BroadcastMessage', (type : string, payload : string) => {
-
-        this
-          .msgs
-          .push({severity: type, summary: payload});
+        this.msgs.push({severity: type, summary: payload});
       });
   }
 }
